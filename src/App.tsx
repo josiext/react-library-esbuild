c0d0bb6df616545ebfbcac6fab1e53bcba195184
José Núñez <jose.nunez.rv@gmail.com>
2022-08-07 16:32:03 -0400

    Update

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, Input } from "lib";
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>Button</h1>
+    <Button label="My button" />
+    <h1>Input</h1>
+    <Input label="My Input" />
+  </div>
+);
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,1 @@
-import React from "react";
-
-interface Props {
-  foo: string;
-}
-
-const App: React.FC<Props> = ({ foo }) => <div>Foo's value is: {foo}</div>;
-
-export default App;
+export * from "./lib";
