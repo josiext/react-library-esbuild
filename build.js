ca2da815a69551f2772a53383f52af38eb743a7a
José Núñez <jose.nunez.rv@gmail.com>
2022-08-07 16:00:49 -0400

    Update

--- build.js
+++ build.js
@@ -5,23 +5,20 @@ const entryFile = "src/index.tsx";
 const shared = {
   bundle: true,
   entryPoints: [entryFile],
-  // Treat all dependencies in package.json as externals to keep bundle size to a minimum
   external: Object.keys(dependencies),
-  logLevel: "info",
   minify: true,
   sourcemap: true,
+  target: ["esnext", "node14"],
 };
 
 build({
   ...shared,
   format: "esm",
   outfile: "./dist/index.esm.js",
-  target: ["esnext", "node12.22.0"],
 });
 
 build({
   ...shared,
   format: "cjs",
   outfile: "./dist/index.cjs.js",
-  target: ["esnext", "node12.22.0"],
 });
