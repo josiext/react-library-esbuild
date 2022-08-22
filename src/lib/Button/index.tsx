ac85afeeb01b11ebbdc142f92f901d8204657605
José Núñez <jose.nunez.rv@gmail.com>
2022-08-22 11:25:01 -0400

    implementar theme ui

--- src/lib/Button/index.tsx
+++ src/lib/Button/index.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+/** @jsxImportSource theme-ui */
 
-export default function Button({ label }: { label: string }) {
-  return <button>{label}</button>;
+import { Button as ButtonThemeUI, ThemeUIStyleObject } from "theme-ui";
+
+export interface ButtonProps {
+  sx?: ThemeUIStyleObject | undefined;
+  variant?: "primary" | "secondary";
+  children: React.ReactNode;
+}
+
+export default function Button({
+  children,
+  sx,
+  variant = "primary",
+}: ButtonProps) {
+  return (
+    <ButtonThemeUI
+      sx={{
+        display: "inline-block",
+        backgroundColor: variant === "primary" ? "red" : "green",
+        fontSize: "50px",
+        cursor: "pointer",
+        ...sx,
+      }}
+    >
+      {children}
+    </ButtonThemeUI>
+  );
 }
